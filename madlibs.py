a75d1799050a0d4711787868e512b5abb0205e0d
faheezat09 <faheeztolaitan09@gmail.com>
2024-08-27 15:33:06 +0100

    new project

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+adj = input("Adjective:")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"computer programming is so {adj}! it makes me so excited all the time because \
+i love to {verb1}. stay hydrated and{verb2} like you are {famous_person}!"
+
+print(madlib)
